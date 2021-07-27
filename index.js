4290e53cee51d10850b5765938376d38563d790b
Ryan Bollettino <ryan.bollettino@gmail.com>
2021-07-27 14:01:36 -0500

    for to wh ile

--- index.js
+++ index.js
@@ -16,17 +16,14 @@ let three = () => {
     document.getElementsByTagName("main")[0].children[2].children[0].children[0].innerText = "Adios!"; 
 }
 
-
-
 const rotation = () => {
     i = 0; 
     while (i < 100) {
-        setTimeout(function() {one()}, 3000); 
-        setTimeout(function() {two()}, 3000); 
-        setTimeout(function() {three()}, 3000); 
+        setTimeout(() => one(), 2000);
+        setTimeout(() => two(), 2000);
+        setTimeout(() => three(), 2000);
         i++
     }
 }
-
 
 rotation()
